224ea37f8f7bc2d2793fee8c42955723b0cd75bb
beyonet <freel.nathanael@gmail.com>
2020-08-15 20:11:36 -0400

    test

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,7 @@ Prototype By: author
 """
 import os
 from flask import Flask
-from flask import render_template
-from flask import request
+from flask import request, render_template
 import csv
 
 
--- app/bench/views.py
+++ app/bench/views.py
@@ -21,3 +21,21 @@ def root():
         fieldnames = [key for key in results[0].keys()]
 
         return render_template('home.html', results=results, fieldnames=fieldnames, len=len)
+
+
+@bench.route('/bs3', methods=['GET', 'POST'])
+def test_bs3():
+    if request.method == 'GET':
+        return render_template('home_bs3.html')
+    elif request.method == 'POST':
+        results = []
+
+        user_csv = request.form.get('user_csv').split('\n')
+        reader = csv.DictReader(user_csv)
+
+        for row in reader:
+            results.append(dict(row))
+
+        fieldnames = [key for key in results[0].keys()]
+
+        return render_template('home.html', results=results, fieldnames=fieldnames, len=len)
